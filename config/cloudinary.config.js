0603d1501579241c129bf52087d59789a285f604
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-06-25 16:16:12 -0500

    final

---
+++ config/cloudinary.config.js
@@ -0,0 +1,30 @@
+//vamos a importar
+
+const cloudinary = require('cloudinary').v2 //le decimos que vamos a usar la version 2.
+const { CloudinaryStorage} = require('multer-storage-cloudinary');
+const multer = require('multer')
+
+//config
+cloudinary.config({
+   cloud_name:process.env.CLOUDINARY_NAME,
+   api_key: process.env.CLOUDINARY_KEY,
+   api_secret:process.env.CLOUDINARY_SECRET,
+});
+
+//config storage
+const storage =new CloudinaryStorage({
+
+    //cloudinary config
+    cloudinary,
+    params:{
+        folder:"edit-user-project",
+        allowed_formats:['jpeg', 'png', 'gif','svg',"jpeg"]
+    }
+});
+
+module.exports = multer({storage})
+
+
+
+
+
---
+++ middleware/customMiddleware.js
@@ -0,0 +1,18 @@
+//creamos un middleware para checar los roles que tienen permitidor acceder o realizar daterminadas acciones
+
+                //["ADMIN,"STAFF"]
+exports.checkRole =(arrayRoles) => {
+
+return (req,res,next)=>{
+    //voy a sacar de mi req.session al user logged para saber qe rol tiene
+
+    const { role} = req.session.user
+    if(arrayRoles.includes(role)){
+        return next()
+    } else {
+        return res.status(403).send("no tienes el nivel requerido para esta accion")
+    }
+
+
+}
+}
